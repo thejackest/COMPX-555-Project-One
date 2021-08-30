6b7edb32aa748246606eaa94af153e88cd6659ff
mharcourt <mharcourt@hotmail.co.nz>
2021-08-30 17:45:30 +1200

    Changed | sleep 1 to && as this will wait until the prior task is complete to run the next

--- AminoAcidGenShellScripts/RunFindAminoAcids.sh
+++ AminoAcidGenShellScripts/RunFindAminoAcids.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ExtractSequence.sh $1 && sleep 1 | ./IsolateSubsequences.sh sequence.txt && sleep 1 | ./GenerateReverseComplement.sh subsequences.txt && sleep 1 | ./IsolateComplementSubsequences.sh reverseComplements.txt && sleep 1 | ./TranslateGenes.sh subsequences.txt
+./ExtractSequence.sh $1 && ./IsolateSubsequences.sh sequence.txt && ./GenerateReverseComplement.sh subsequences.txt && ./IsolateComplementSubsequences.sh reverseComplements.txt && ./TranslateGenes.sh subsequences.txt
